2f88f30e8a3672df4de95c80dacf0a02f70fe90b
Anass N'DRI <anassndri@live.fr>
2024-07-13 11:34:14 +0200

    config to deploy

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-// Servir les fichiers statiques de l'application Angular depuis le dossier 'frontend/dist/mm-consulting'
-app.use(express.static(path.join(__dirname, "dist/ng-pokemon-app")));
+// Serve the static files from the Angular app
+app.use(express.static(path.join(__dirname, 'dist/ng-pokemon-app')));
 
-// Rediriger toutes les requêtes vers le fichier index.html de l'application Angular
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "dist/ng-pokemon-app/index.html"));
+// Redirect all routes to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/ng-pokemon-app/index.html'));
 });
 
-// Démarrer l'application sur le port défini par Heroku ou sur le port 8080 en local
+// Start the app by listening on the default Heroku port
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
