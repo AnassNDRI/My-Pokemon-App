7db273a282b76436629179ac6fe3e5760cfb18ff
Anass N'DRI <anassndri@live.fr>
2024-07-13 10:17:49 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 // Servir les fichiers statiques de l'application Angular depuis le dossier 'frontend/dist/mm-consulting'
-app.use(express.static(path.join(__dirname, 'dist/ng-pokemon-app')));
+app.use(express.static(path.join(__dirname, "dist/ng-pokemon-app")));
 
 // Rediriger toutes les requêtes vers le fichier index.html de l'application Angular
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/ng-pokemon-app/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist/ng-pokemon-app/index.html"));
 });
 
 // Démarrer l'application sur le port défini par Heroku ou sur le port 8080 en local
